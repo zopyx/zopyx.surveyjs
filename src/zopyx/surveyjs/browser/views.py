76d8eb8f134c8b460e1e2a7bc7b51be749d1a06e
Andreas Jung <info@zopyx.com>
2021-11-28 11:37:30 +0100

    load editor JSON from Plone

--- src/zopyx/surveyjs/browser/views.py
+++ src/zopyx/surveyjs/browser/views.py
@@ -5,8 +5,11 @@ from Products.Five import BrowserView
 class Views(BrowserView):
 
     def get_form_json(self):
+        """ JSON for SurveyJS renderer """
 
         json_form = self.context.form_json
+
+        # verify JSON validity
         data = json.loads(json_form)
 
         self.request.response.setHeader("content-type", "application/json")
