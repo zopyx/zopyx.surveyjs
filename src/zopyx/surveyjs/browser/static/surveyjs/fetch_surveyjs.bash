a120efd37af350b95b7825e6047d1d70abab29da
Andreas Jung <info@zopyx.com>
2026-01-22 16:25:48 +0100

    added

---
+++ src/zopyx/surveyjs/browser/static/surveyjs/fetch_surveyjs.bash
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+wget https://unpkg.com/jspdf@2.5.1/dist/jspdf.umd.min.js
+wget https://unpkg.com/plotly.js-dist-min/plotly.min.js
+wget https://unpkg.com/survey-analytics/survey.analytics.min.css
+wget https://unpkg.com/survey-analytics/survey.analytics.min.js
+wget https://unpkg.com/survey-core/survey-core.min.css
+wget https://unpkg.com/survey-core/survey.core.min.js
+wget https://unpkg.com/survey-core/survey.i18n.min.js
+wget https://unpkg.com/survey-core/themes/contrast-dark.min.js
+wget https://unpkg.com/survey-core/themes/contrast-light.min.js
+wget https://unpkg.com/survey-core/themes/index.min.js
+wget https://unpkg.com/survey-creator-core/i18n/${portal_state/language}.js
+wget https://unpkg.com/survey-creator-core/survey-creator-core.min.css
+wget https://unpkg.com/survey-creator-core/survey-creator-core.min.js
+wget https://unpkg.com/survey-creator-js/survey-creator-js.min.js
+wget https://unpkg.com/survey-js-ui/survey-js-ui.min.js
+wget https://unpkg.com/survey-pdf/survey.pdf.min.js
