07b4cdcbedcb96fee24f0307373a177bba354b38
Andreas Jung <info@zopyx.com>
2026-03-05 15:12:29 +0100

    added

---
+++ src/zopyx/surveyjs/permissions.py
@@ -0,0 +1,11 @@
+# Permission title strings for use with plone.api.user.has_permission()
+# and getSecurityManager().checkPermission().
+# Variable names mirror the permission IDs defined in permissions.zcml.
+
+# Custom permissions
+AddSurvey = "zopyx.surveyjs: Add Survey"
+AddSurveyTemplate = "zopyx.surveyjs: Add Survey Template"
+
+# Standard Plone/CMF permissions (re-exported for convenience)
+ManagePortal = "Manage portal"
+ModifyPortalContent = "Modify portal content"
