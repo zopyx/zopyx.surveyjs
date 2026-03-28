a8af75480c64c1c717f3d7a437c4ced152115e56
Andreas Jung <info@zopyx.com>
2026-03-28 13:15:07 +0100

    updated

--- setup.py
+++ setup.py
@@ -82,7 +82,7 @@ setup(
             "plone.testing>=5.0.0",
             "plone.app.contenttypes",
             "plone.app.robotframework[debug]",
-            "collective.z3cform.jsonwidget",
+#            "collective.z3cform.jsonwidget",
             "orjson",
             "diskcache",
             "polib",
