11f753108d912aaa0c2b54d37cef03721d40680a
Andreas Jung <info@zopyx.com>
2026-06-09 15:55:00 +0200

    feat: generate authenticity_token_secret on install and add content type SVG icons
    - Wire uuid4 authenticity_token_secret generation into post_install handler
      so every fresh install gets a secure random secret by default
    - Add white SVG icons for Form/Survey and Form/Survey Template content
      types, served via ++resource++zopyx.surveyjs
    - Set icon_expr on both FTIs so the Plone "Add new" toolbar menu displays
      proper icons instead of placeholder dots
    - Add upgrade step 1000→1001 to apply icon_expr to existing sites
    - Bump GenericSetup metadata version to 1001
    
    Closes: #placeholder

--- src/zopyx/surveyjs/setuphandlers.py
+++ src/zopyx/surveyjs/setuphandlers.py
@@ -1,6 +1,8 @@
-# -*- coding: utf-8 -*-
+from plone import api
+from plone.api.exc import InvalidParameterError
 from Products.CMFPlone.interfaces import INonInstallable
 from zope.interface import implementer
+import uuid
 
 
 @implementer(INonInstallable)
@@ -14,7 +16,41 @@ class HiddenProfiles(object):
 
 def post_install(context):
     """Post install script"""
-    # Do something at the end of the installation of this package.
+    _ensure_authenticity_token_secret()
+
+
+def _ensure_authenticity_token_secret():
+    """Set a uuid4 authenticity_token_secret if none is set."""
+    secret_key = (
+        "zopyx.surveyjs.interfaces.IFormsSettings.authenticity_token_secret"
+    )
+    enabled_key = (
+        "zopyx.surveyjs.interfaces.IFormsSettings.authenticity_token_enabled"
+    )
+    try:
+        current = api.portal.get_registry_record(secret_key)
+        if current:
+            return
+        api.portal.set_registry_record(secret_key, str(uuid.uuid4()))
+        api.portal.set_registry_record(enabled_key, True)
+    except InvalidParameterError:
+        pass
+
+
+def upgrade_1000_to_1001(context):
+    """Upgrade step: set icon_expr on Survey and SurveyTemplate FTIs."""
+    from Products.CMFCore.utils import getToolByName
+    portal_url = getToolByName(context, "portal_url")()
+    pt = getToolByName(context, "portal_types")
+    for type_id, icon_name in [
+        ("Survey", "survey-icon.svg"),
+        ("SurveyTemplate", "survey-template-icon.svg"),
+    ]:
+        fti = pt.get(type_id)
+        if fti is not None:
+            fti.icon_expr = (
+                f"{portal_url}/++resource++zopyx.surveyjs/{icon_name}"
+            )
 
 
 def uninstall(context):
